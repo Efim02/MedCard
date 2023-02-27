3b4c941d860bdad24b79a23ce6eaa0979fa0f72b
TommyBrenson <tommy.brenson@yandex.ru>
2023-02-27 22:42:04 +0700

    chore: organized and configured environment for frontend development

---
+++ client/src/app/routes/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import NotFound from '../pages/NotFoundPage';
+
+const AppRouter = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={ <HomePage /> } />
+                <Route path="*" element={ <NotFound /> } />
+            </Routes>
+        </>
+    )
+}
+
+export default AppRouter;
